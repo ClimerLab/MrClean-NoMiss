e90ba192f8e7e811dfb0a355d7ce176cf8ac8834
Ken Smith <kpsmithjr@gmail.com>
2023-06-28 10:35:54 -0500

    Added summary logging

--- src/CalcPairsWrapper.cpp
+++ src/CalcPairsWrapper.cpp
@@ -21,13 +21,27 @@ int main(int argc, char* argv[]) {
   }
 
   Timer timer;
-
+  timer.start();
   // Read in data
   DataContainer data(data_file, na_symbol, num_header_rows, num_header_cols);
 
   CalcPairs calcPairs(data);
   calcPairs.calc_free_row_pairs();
   calcPairs.calc_free_col_pairs();
+
+  timer.stop();
+
+  std::string file_name = "CalcPairs.csv";
+  FILE* output;
+  if((output = fopen(file_name.c_str(), "a+")) == nullptr) {
+    fprintf(output, "ERROR - Could not open file (%s)\n", file_name.c_str());
+    exit(1);
+  }
+  fprintf(output, "%s,%d\n", data_file.c_str(), timer.elapsed_cpu_time());
+  fclose(output);
+
+  fprintf(stderr, "Summary of CalcPairs\n");
+  fprintf(stderr, "\tTook %lf seconds\n", timer.elapsed_cpu_time());
 
   return 0;
 }
